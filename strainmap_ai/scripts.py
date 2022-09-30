9962072a29be4ad0224c6ac64270b390e9d3be43
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-09-30 05:28:34 +0100

    Add normalisation step

--- strainmap_ai/scripts.py
+++ strainmap_ai/scripts.py
@@ -5,7 +5,7 @@ from typing import Optional
 import numpy as np
 
 from .readers import load_data
-from .unet import DataAugmentation, UNet
+from .unet import DataAugmentation, Normal, UNet
 
 logger = logging.getLogger(__name__)
 
@@ -16,17 +16,23 @@ def train(filenames: Path, model_file: Optional[Path] = None) -> None:
     # read CSV file
     logger.info("Starting data loading...")
     data = load_data(filenames)
+    logger.info("... data loading complete!")
 
-    logger.info("Data loading complete! Starting pre-processing...")
+    logger.info("Starting pre-processing...")
     augmented = DataAugmentation.factory().augment(data)
-    logger.info("Data pre-processing complete!")
+    logger.info("... data pre-processing complete!")
 
-    # TODO: Normalisation step
+    # Normalise the data
+    logger.info("Starting data normalisation...")
+    labels = augmented.sel(comp="LABELS").data
+    images = Normal().run(
+        augmented.sel(comp=["MAG", "X", "Y", "Z"]).data, method="zeromean_unitvar"
+    )
+    logger.info("... data normalisation complete!")
 
     # Model training
+    logger.info("Starting AI training (this might take a while)...")
     model = UNet.factory()
     model.compile_model()
-    labels = np.array(augmented.sel(comp="LABELS").data)
-    images = np.array(augmented.sel(comp=["MAG", "X", "Y", "Z"]).data)
-    model.train(images, labels, model_file)
+    model.train(np.asarray(images), np.asarray(labels), model_file)
     logger.info(f"Training complete! Teained model saved to {model_file}")
